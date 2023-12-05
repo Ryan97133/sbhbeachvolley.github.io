65384613b7415beb35edd5911a3834894dfdb8fe
Ryan97133 <ryanpeter97133@gmail.com>
2023-12-05 18:32:10 -0400

    Add JS nav scroll

--- assets/styles/style.js
+++ assets/styles/style.js
@@ -2,14 +2,28 @@
 
 const topButton = document.getElementById("btn-top");
 
+const nav = document.querySelector("nav");
+
 // BTN TOP SCROLL TOP
 window.addEventListener("scroll", () => {
   // Test fonction------------------------------------------
-  console.log(document.scrollY);
+  // console.log(document.scrollY);
   //   -------------------------------------------------------
-  if (window.scrollY > 50) {
+  if (window.scrollY > 80) {
     topButton.style.right = "30px";
   } else {
     topButton.style.right = "-50px";
+  }
+});
+
+// NAV SCROLL DOWN
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 65) {
+    nav.style.top = "0";
+    nav.style.position = "sticky";
+    nav.style.zIndex = "1";
+  } else if (window.scrollY < 65) {
+    nav.style.top = "-500px";
   }
 });
