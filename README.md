b0f310eba7278542b4b6fac4c739d117ea1f548f
Ryan97133 <ryanpeter97133@gmail.com>
2023-12-07 16:11:10 -0400

    Test JS NAV Full

--- README.md
+++ README.md
@@ -1,23 +1,17 @@
 # sbhbeachvolley.github.io
+
 Projet de création d'un site WEB pour le Saint Barth Beach Volley
 
-# Epuré le site WEB
+# A faire
 
-1 - Créer une première page de garde comprenant : 
-
-- Header : Créer une vidéo de présentation pour l'association
-
-- Section : Présentation +  Actualités
-
-2 - Créer un fichier html par élement dans la balise nav
+1 - Créer un fichier html par élement dans la balise nav
 
 - Histoire
+
 - Pratique
 
 - Evènement
 
-- Incription
-
-- Contact
-
 - Boutique
+
+2 -
--- assets/styles/style.js
+++ assets/styles/style.js
@@ -5,6 +5,12 @@ const header = document.querySelector("header");
 const nav = document.querySelector("nav");
 const btnNav1 = document.getElementById("btn-nav1");
 const menuItem1 = document.querySelector(".menu-item1");
+const btnNav2 = document.getElementById("btn-nav2");
+const menuItem2 = document.querySelector(".menu-item2");
+const btnNav3 = document.getElementById("btn-nav3");
+const menuItem3 = document.querySelector(".menu-item3");
+const btnNav4 = document.getElementById("btn-nav4");
+const menuItem4 = document.querySelector(".menu-item4");
 
 // BTN TOP SCROLL TOP
 window.addEventListener("scroll", () => {
@@ -31,47 +37,84 @@ window.addEventListener("scroll", () => {
 });
 
 //NAV MENU-----------------------------------------------------
+window.addEventListener("mouseout", (e) => {
+  if ((e.target = header)) {
+    menuItem1.style.display = "none";
+    menuItem2.style.display = "none";
+    menuItem3.style.display = "none";
+    menuItem4.style.display = "none";
+  }
+});
 
-// btnNav1.addEventListener("mousemove", (e) => {
-//   if (e.target === btnNav1 || (e.target = menuItem1)) {
-//     menuItem1.style.display = "block";
-//     menuItem1.style.zIndex = "1";
-//   } else {
-//     menuItem1.style.display = "none";
-//   }
-// });
-
+// NAV MENU 1
 btnNav1.addEventListener("mouseenter", (e) => {
   if ((e.target = btnNav1)) {
     menuItem1.style.display = "block";
     menuItem1.style.zIndex = "1";
-    console.log(btnNav1);
   }
 });
 menuItem1.addEventListener("mouseover", (e) => {
   if ((e.target = menuItem1)) {
     menuItem1.style.display = "block";
     menuItem1.style.zIndex = "1";
-    console.log(menuItem1);
   }
 });
 menuItem1.addEventListener("mouseout", (e) => {
   if ((e.target = menuItem1)) {
     menuItem1.style.display = "none";
-    console.log(menuItem1);
   }
 });
-
-// header.addEventListener("mousemove", (e) => {
-//   if (
-//     e.pageY > 230 ||
-//     (e.pageX < 130 && e.pageY < 230) ||
-//     (e.pageY < 230 && e.pageX > 365)
-//   ) {
-//     menuItem1.style.display = "none";
-//     console.log(btnNav1);
-//   } else {
-//     menuItem1.style.display = "block";
-//     menuItem1.style.zIndex = "1";
-//   }
-// });
+// NAV MENU 2
+btnNav2.addEventListener("mouseenter", (e) => {
+  if ((e.target = btnNav2)) {
+    menuItem2.style.display = "block";
+    menuItem2.style.zIndex = "1";
+  }
+});
+menuItem2.addEventListener("mouseover", (e) => {
+  if ((e.target = menuItem2)) {
+    menuItem2.style.display = "block";
+    menuItem2.style.zIndex = "1";
+  }
+});
+menuItem2.addEventListener("mouseout", (e) => {
+  if ((e.target = menuItem2)) {
+    menuItem2.style.display = "none";
+  }
+});
+// NAV MENU 3
+btnNav3.addEventListener("mouseenter", (e) => {
+  if ((e.target = btnNav3)) {
+    menuItem3.style.display = "block";
+    menuItem3.style.zIndex = "1";
+  }
+});
+menuItem3.addEventListener("mouseover", (e) => {
+  if ((e.target = menuItem3)) {
+    menuItem3.style.display = "block";
+    menuItem3.style.zIndex = "1";
+  }
+});
+menuItem3.addEventListener("mouseout", (e) => {
+  if ((e.target = menuItem3)) {
+    menuItem3.style.display = "none";
+  }
+});
+// NAV MENU 4
+btnNav4.addEventListener("mouseenter", (e) => {
+  if ((e.target = btnNav4)) {
+    menuItem4.style.display = "block";
+    menuItem4.style.zIndex = "1";
+  }
+});
+menuItem4.addEventListener("mouseover", (e) => {
+  if ((e.target = menuItem4)) {
+    menuItem4.style.display = "block";
+    menuItem4.style.zIndex = "1";
+  }
+});
+menuItem4.addEventListener("mouseout", (e) => {
+  if ((e.target = menuItem4)) {
+    menuItem4.style.display = "none";
+  }
+});
