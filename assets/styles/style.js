652649ef5fc4bcb8452310af451b2e862cca2446
Ryan97133 <ryanpeter97133@gmail.com>
2023-12-07 14:15:30 -0400

    Test JS nav first btn

--- assets/styles/style.js
+++ assets/styles/style.js
@@ -1,13 +1,15 @@
 // CONSTANTE
 
 const topButton = document.getElementById("btn-top");
-
+const header = document.querySelector("header");
 const nav = document.querySelector("nav");
+const btnNav1 = document.getElementById("btn-nav1");
+const menuItem1 = document.querySelector(".menu-item1");
 
 // BTN TOP SCROLL TOP
 window.addEventListener("scroll", () => {
-  // Test fonction------------------------------------------
-  // console.log(document.scrollY);
+  // Test fonction--------------------------------------------
+  console.log(document.scrollY);
   //   -------------------------------------------------------
   if (window.scrollY > 65) {
     topButton.style.right = "30px";
@@ -27,3 +29,49 @@ window.addEventListener("scroll", () => {
     nav.style.top = "-500px";
   }
 });
+
+//NAV MENU-----------------------------------------------------
+
+// btnNav1.addEventListener("mousemove", (e) => {
+//   if (e.target === btnNav1 || (e.target = menuItem1)) {
+//     menuItem1.style.display = "block";
+//     menuItem1.style.zIndex = "1";
+//   } else {
+//     menuItem1.style.display = "none";
+//   }
+// });
+
+btnNav1.addEventListener("mouseenter", (e) => {
+  if ((e.target = btnNav1)) {
+    menuItem1.style.display = "block";
+    menuItem1.style.zIndex = "1";
+    console.log(btnNav1);
+  }
+});
+menuItem1.addEventListener("mouseover", (e) => {
+  if ((e.target = menuItem1)) {
+    menuItem1.style.display = "block";
+    menuItem1.style.zIndex = "1";
+    console.log(menuItem1);
+  }
+});
+menuItem1.addEventListener("mouseout", (e) => {
+  if ((e.target = menuItem1)) {
+    menuItem1.style.display = "none";
+    console.log(menuItem1);
+  }
+});
+
+// header.addEventListener("mousemove", (e) => {
+//   if (
+//     e.pageY > 230 ||
+//     (e.pageX < 130 && e.pageY < 230) ||
+//     (e.pageY < 230 && e.pageX > 365)
+//   ) {
+//     menuItem1.style.display = "none";
+//     console.log(btnNav1);
+//   } else {
+//     menuItem1.style.display = "block";
+//     menuItem1.style.zIndex = "1";
+//   }
+// });
