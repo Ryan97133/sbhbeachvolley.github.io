7c286caa61a7fc1b464214a3953dff1d4af67e61
Ryan97133 <ryanpeter97133@gmail.com>
2023-12-12 10:27:21 -0400

    Add 2 part for BV

--- nav/layoutH/BeachVolley/assets/styles/style.js
+++ nav/layoutH/BeachVolley/assets/styles/style.js
@@ -13,6 +13,26 @@ const menuItem3 = document.querySelector(".menu-item3");
 const btnNav4 = document.getElementById("btn-nav4");
 const menuItem4 = document.querySelector(".menu-item4");
 
+// SOMMAIRE
+
+const Som_1 = document.getElementById("Som_1");
+const Som_1a = document.getElementById("Som_1a");
+
+const Som_2 = document.getElementById("Som_2");
+const Som_2a = document.getElementById("Som_2a");
+
+const Som_3 = document.getElementById("Som_3");
+const Som_3a = document.getElementById("Som_3a");
+
+const Som_4 = document.getElementById("Som_4");
+const Som_4a = document.getElementById("Som_4a");
+
+const Som_5 = document.getElementById("Som_5");
+const Som_5a = document.getElementById("Som_5a");
+
+const Som_6 = document.getElementById("Som_6");
+const Som_6a = document.getElementById("Som_6a");
+
 // BTN TOP SCROLL TOP
 window.addEventListener("scroll", () => {
   if (window.scrollY > 65) {
@@ -38,6 +58,12 @@ btnNav1.addEventListener("mouseenter", (e) => {
   if ((e.target = btnNav1)) {
     menuItem1.style.display = "block";
     menuItem1.style.zIndex = "1";
+    btnNav1.style.color = "black";
+  }
+});
+btnNav1.addEventListener("mouseout", (e) => {
+  if ((e.target = btnNav1)) {
+    btnNav1.style.color = "white";
   }
 });
 menuItem1.addEventListener("mouseover", (e) => {
@@ -45,12 +71,14 @@ menuItem1.addEventListener("mouseover", (e) => {
     menuItem1.style.display = "block";
     menuItem1.style.zIndex = "1";
     btnNav1.style.background = "white";
+    btnNav1.style.color = "black";
   }
 });
 menuItem1.addEventListener("mouseout", (e) => {
   if ((e.target = menuItem1)) {
     menuItem1.style.display = "none";
     btnNav1.style.background = "none";
+    btnNav1.style.color = "white";
   }
 });
 // NAV MENU 2
@@ -58,6 +86,12 @@ btnNav2.addEventListener("mouseenter", (e) => {
   if ((e.target = btnNav2)) {
     menuItem2.style.display = "block";
     menuItem2.style.zIndex = "1";
+    btnNav2.style.color = "black";
+  }
+});
+btnNav2.addEventListener("mouseout", (e) => {
+  if ((e.target = btnNav2)) {
+    btnNav2.style.color = "white";
   }
 });
 menuItem2.addEventListener("mouseover", (e) => {
@@ -65,12 +99,14 @@ menuItem2.addEventListener("mouseover", (e) => {
     menuItem2.style.display = "block";
     menuItem2.style.zIndex = "1";
     btnNav2.style.background = "white";
+    btnNav2.style.color = "black";
   }
 });
 menuItem2.addEventListener("mouseout", (e) => {
   if ((e.target = menuItem2)) {
     menuItem2.style.display = "none";
     btnNav2.style.background = "none";
+    btnNav2.style.color = "white";
   }
 });
 // NAV MENU 3
@@ -78,6 +114,12 @@ btnNav3.addEventListener("mouseenter", (e) => {
   if ((e.target = btnNav3)) {
     menuItem3.style.display = "block";
     menuItem3.style.zIndex = "1";
+    btnNav3.style.color = "black";
+  }
+});
+btnNav3.addEventListener("mouseout", (e) => {
+  if ((e.target = btnNav3)) {
+    btnNav3.style.color = "white";
   }
 });
 menuItem3.addEventListener("mouseover", (e) => {
@@ -85,12 +127,14 @@ menuItem3.addEventListener("mouseover", (e) => {
     menuItem3.style.display = "block";
     menuItem3.style.zIndex = "1";
     btnNav3.style.background = "white";
+    btnNav3.style.color = "black";
   }
 });
 menuItem3.addEventListener("mouseout", (e) => {
   if ((e.target = menuItem3)) {
     menuItem3.style.display = "none";
     btnNav3.style.background = "none";
+    btnNav3.style.color = "white";
   }
 });
 // NAV MENU 4
@@ -98,6 +142,12 @@ btnNav4.addEventListener("mouseenter", (e) => {
   if ((e.target = btnNav4)) {
     menuItem4.style.display = "block";
     menuItem4.style.zIndex = "1";
+    btnNav4.style.color = "black";
+  }
+});
+btnNav4.addEventListener("mouseout", (e) => {
+  if ((e.target = btnNav4)) {
+    btnNav4.style.color = "white";
   }
 });
 menuItem4.addEventListener("mouseover", (e) => {
@@ -105,11 +155,48 @@ menuItem4.addEventListener("mouseover", (e) => {
     menuItem4.style.display = "block";
     menuItem4.style.zIndex = "1";
     btnNav4.style.background = "white";
+    btnNav4.style.color = "black";
   }
 });
 menuItem4.addEventListener("mouseout", (e) => {
   if ((e.target = menuItem4)) {
     menuItem4.style.display = "none";
     btnNav4.style.background = "none";
+    btnNav4.style.color = "white";
+  }
+});
+
+// SOMMAIRE EVENT
+
+// Sommaire 1
+window.addEventListener("mouseover", (e) => {
+  if (e.pageY < 800) {
+    Som_1.style.background = "rgb(7,71,84, .3)";
+    Som_1a.style.color = "white";
+  } else if (e.pageY > 800) {
+    Som_1.style.background = "none";
+    Som_1a.style.color = "rgba(0, 0, 0, .8)";
+  }
+});
+
+// Sommaire 2
+window.addEventListener("mouseover", (e) => {
+  if (e.pageY > 800 && e.pageY < 1500) {
+    Som_2.style.background = "rgb(7,71,84, .3)";
+    Som_2a.style.color = "white";
+  } else {
+    Som_2.style.background = "none";
+    Som_2a.style.color = "rgba(0, 0, 0, .8)";
+  }
+});
+
+// Sommaire 3
+window.addEventListener("mouseover", (e) => {
+  if (e.pageY > 1500 && e.pageY < 2100) {
+    Som_3.style.background = "rgb(7,71,84, .3)";
+    Som_3a.style.color = "white";
+  } else {
+    Som_3.style.background = "none";
+    Som_3a.style.color = "rgba(0, 0, 0, .8)";
   }
 });
